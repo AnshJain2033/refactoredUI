68bfc12e5afb5188d0bb6d5dc4bf81680ff61e3b
Surabhi Bhutada <surabhi.mtech2022@gmail.com>
2024-06-03 18:51:56 +0530

    DIS UI code

---
+++ dis/src/app/components/common-dashboard/common-dashboard.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { CalendarOptions } from '@fullcalendar/core';
+import dayGridPlugin from '@fullcalendar/daygrid';
+
+@Component({
+  selector: 'app-common-dashboard',
+  templateUrl: './common-dashboard.component.html',
+  styleUrls: ['./common-dashboard.component.scss']
+})
+export class CommonDashboardComponent {
+
+  
+  calendarOptions: CalendarOptions = {
+    plugins: [dayGridPlugin],
+    height: 440,
+    initialView: 'dayGridMonth',
+    events: [
+      { title: 'event 1', date: '2019-04-01' },
+      { title: 'event 2', date: '2019-04-02' },
+    ],
+  };
+
+  constructor() {}
+
+  handleDateClick(arg: any) {
+    alert('date click! ' + arg.dateStr);
+  }
+}
